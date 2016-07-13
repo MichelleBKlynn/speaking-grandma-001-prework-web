f0cb6703a050dc50e1474e816d3957dbe75077f3
Michelle Lynn <MichelleBKlynn@users.noreply.github.com>
2016-07-13 17:31:46 -0400

    Updated grandma.rb

--- grandma.rb
+++ grandma.rb
@@ -1,29 +1,7 @@
-def speak_to_grandma(x)
-  if x == "Hi Nana, how are you?"
-   puts "HUH?! SPEAK UP, SONNY!"
-  end
-
-  if x == "Hi!"
-   puts "HUH?! SPEAK UP, SONNY!"
-  end
-
-  if x == "WHAT DID YOU EAT TODAY?" 
-   puts "NO, NOT SINCE 1938!"
-  end
-
-  if x == "WHAT?"
+def speak_to_grandma(says)
+  if says == says.upcase
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
-
-
-  # Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
